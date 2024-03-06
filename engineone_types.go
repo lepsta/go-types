4b01ed7322d014c547b015d1ddef111e4efe118e
Sithembiso Khumalo <sithembiso@lepsta.com>
2024-03-06 11:06:29 +0200

    Fixes the missing type error

--- engineone_types.go
+++ engineone_types.go
@@ -1,20 +1,20 @@
 package types
 
 type EngineOneTask struct {
-	ID             string                      `json:"id" yaml:"id" validate:"required"`
-	Name           string                      `json:"name" yaml:"name"`
-	Description    string                      `json:"description" yaml:"description"`
-	Executor       string                      `json:"executor" yaml:"executor" validate:"required"`
-	DependsOn      []string                    `json:"dependsOn" yaml:"dependsOn"`
-	GlobalInput    interface{}                 `json:"globalInput,omitempty" yaml:"globalInput,omitempty"`
-	Input          interface{}                 `json:"input,omitempty" yaml:"input,omitempty" validate:"required"`
-	Output         Output                      `json:"output,omitempty" yaml:"output,omitempty"`
-	Duration       int64                       `json:"duration,omitempty" yaml:"duration,omitempty"`
-	LatestDuration int64                       `json:"latestDuration,omitempty" yaml:"latestDuration,omitempty"`
-	IsExecuting    bool                        `yaml:"isExecuting" json:"isExecuting"`
-	Succeeded      bool                        `yaml:"succeeded" json:"succeeded"`
-	NextTaskId     string                      `json:"nextTaskId,omitempty" yaml:"nextTaskId,omitempty"`
-	Error          error                       `json:"error,omitempty" yaml:"error,omitempty"`
+	ID             string                    `json:"id" yaml:"id" validate:"required"`
+	Name           string                    `json:"name" yaml:"name"`
+	Description    string                    `json:"description" yaml:"description"`
+	Executor       string                    `json:"executor" yaml:"executor" validate:"required"`
+	DependsOn      []string                  `json:"dependsOn" yaml:"dependsOn"`
+	GlobalInput    interface{}               `json:"globalInput,omitempty" yaml:"globalInput,omitempty"`
+	Input          interface{}               `json:"input,omitempty" yaml:"input,omitempty" validate:"required"`
+	Output         interface{}               `json:"output,omitempty" yaml:"output,omitempty"`
+	Duration       int64                     `json:"duration,omitempty" yaml:"duration,omitempty"`
+	LatestDuration int64                     `json:"latestDuration,omitempty" yaml:"latestDuration,omitempty"`
+	IsExecuting    bool                      `yaml:"isExecuting" json:"isExecuting"`
+	Succeeded      bool                      `yaml:"succeeded" json:"succeeded"`
+	NextTaskId     string                    `json:"nextTaskId,omitempty" yaml:"nextTaskId,omitempty"`
+	Error          error                     `json:"error,omitempty" yaml:"error,omitempty"`
+	Dependencies   map[string]*EngineOneTask `json:"-" yaml:"-"`
 	// Condition      func(input TaskOutput) bool `json:"-" yaml:"-"`
-	Dependencies   map[string]*Task            `json:"-" yaml:"-"`
 }
